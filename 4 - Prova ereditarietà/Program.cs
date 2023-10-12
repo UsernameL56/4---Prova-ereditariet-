946e2c081424c872399771ff4a0cf5fd4467fc19
corsi.20188 <corsi.20188@E2-9-PC05>
2023-10-12 10:05:40 +0200

    Aggiungere i file di progetto.

---
+++ 4 - Prova ereditarietà/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___Prova_ereditarietà
+{
+    class Quadrato
+    {
+        // attributi
+        protected int _lato;
+
+        // costruttore
+        public Quadrato(int _lato) 
+        {
+            Lato = _lato;
+        }
+
+        // metodi accessor
+        public int Lato
+        {
+            get { return _lato; }
+            set { _lato = value; }
+        }
+
+        // metodi
+        public int CalcoloArea()
+        {
+
+            return _lato * _lato;
+        }
+        public int CalcoloPerimetro()
+        {
+            return _lato * 4;
+        }
+    }
+
+    class Cubo : Quadrato
+    {
+        public Cubo(int _lato) : base(_lato)
+        {
+
+        }
+
+        public int CalcoloSuperficie()
+        {
+            return 6 * CalcoloArea();
+        }
+        public int CalcoloVolume()
+        {
+            return _lato * _lato * _lato;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il lato: ");
+            int lato = int.Parse(Console.ReadLine());
+            Cubo c = new Cubo(lato);
+
+
+            Console.WriteLine($"L'area è: {c.CalcoloArea()}, il perimetro è: {c.CalcoloPerimetro()} ");
+            Console.WriteLine($"La superficie tot è: {c.CalcoloSuperficie()}, il volume è: {c.CalcoloVolume()} ");
+        }
+    }
+}
